144140eb52dbb469c08eeda62b6cc0417d38406b
dstyle0210 <dstyle0210@gmail.com>
2023-07-12 11:39:48 +0900

    해봐

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,6 +1,10 @@
 import {task} from "gulp";
+import {chromium,devices} from "playwright";
 
-task("test",(done) => {
-    console.log("test");
+task("test",async (done) => {
+    const browser = await chromium.launch({headless:true});
+    const page = await browser.newPage();
+    await page.goto("https://www.naver.com");
+    console.log(page.url());
     done();
 });
