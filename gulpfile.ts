1fe7d0e14930c9861a835b1210007db30d0cab9f
dstyle0210 <dstyle0210@gmail.com>
2023-07-12 13:12:00 +0900

    다시다시

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,6 +5,9 @@ task("test",async (done) => {
     const browser = await chromium.launch({headless:true});
     const page = await browser.newPage();
     await page.goto("https://www.naver.com");
+
+    const document = {getElementsByTagName:(name:string) => [{innerHTML:"test"}]}; // github action 용 회피타입
+
     const title = await page.evaluate(() => {
         return document.getElementsByTagName("title")[0].innerHTML;
     });
