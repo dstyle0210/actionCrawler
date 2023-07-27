5242e5508f0be1b5888ee2437df0d99b97508103
dstyle0210 <dstyle0210@gmail.com>
2023-07-27 15:15:15 +0900

    다시 기억되살리기

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # actionCrawler
+크롤링을 통해서 매일 푸쉬받을 정보를 만드는중..   
+(뭘 만들지는 아직 미정..)
 
+```powershell
+> Set-ExecutionPolicy RemoteSigned
+```
+
+```console
+> git config --global user.name dstyle0210
+> git config --global user.email dstyle0210@gmail.com
+```
--- gulpfile.ts
+++ gulpfile.ts
@@ -16,14 +16,15 @@ task("test",async (done) => {
         return document.querySelectorAll(".cjs_t")[0].innerHTML
     });
 
-    const bot = new TelegramBot(token, {polling: false});
-    bot.sendMessage(chatId, title);
-    console.log(title);
-    fs.writeFileSync("list.txt",title,"utf8");
+    // TOOD : firebase에 데이터 저장시키고.. 뭘 저장하지?
 
-    await new Promise((resolve)=>setTimeout(resolve,660000)); // 11분 후 닫음
+    const bot = new TelegramBot(token, {polling: false});
+    bot.sendMessage(chatId, "매일경제 https://media.naver.com/press/009/newspaper"); // 매일경제 신문보기 화면
+    
+    await new Promise((resolve)=>setTimeout(resolve,660000)); // 11분 후 닫음 (텔레그램은 연결 후 10분 이내 끊을경우 429에러 발생함 , Error: ETELEGRAM: 429 Too Many Requests: retry after 599)
+    // await new Promise((resolve)=>setTimeout(resolve,1000)); // 개발용 1초 있다가 닫음 (그냥 에러 나는걸로..)
     await browser.close();
-    bot.sendMessage(chatId, "닫습니다.2");
+    bot.sendMessage(chatId, "닫습니다.");
     await bot.close();
     done();
 });
