8bc9cd23888582eadaa75ee81ba72385e0dd8b59
dstyle0210 <dstyle0210@gmail.com>
2023-08-08 09:39:21 +0900

    소스변경 : NB 미수급

--- gulpfile.ts
+++ gulpfile.ts
@@ -8,12 +8,11 @@ task("test",async (done) => {
     const browser = await chromium.launch({headless:true});
     const page = await browser.newPage();
     
-
-    
     // 나이키 발매정보
     await page.goto("https://www.nike.com/kr/launch?s=upcoming");
     const nikeLaunchList:{name:string,link:string}[] = await page.evaluate(() => {
         var prdList = document.querySelectorAll(".product-card");
+        if(!prdList.length) return [{name:"오늘 발매없음",link:""}];
         const todayLaunchList:{name:string,link:string}[] = [];
         prdList.forEach((productCard) => {
             const nowDate = new Date();nowDate.setHours(nowDate.getHours() + 9); // github action UTC+0
@@ -29,18 +28,16 @@ task("test",async (done) => {
                 }
             };
         });
+        if(!todayLaunchList.length) return [{name:"데이터 미수급 에러",link:""}];
         return todayLaunchList;
     });
 
     // 텔레그램봇 시작
     const bot = new TelegramBot(token, {polling: false});
-    if(nikeLaunchList.length){
-        // 오늘발매 있음
-        for(let card of nikeLaunchList){
-            bot.sendMessage(chatId, "[SNKRS] "+card.name+"\n"+card.link);
-        };
-    }else{
-        bot.sendMessage(chatId, "[SNKRS] 나이키 오늘 발매없음");
+
+    // 텔레그램 발송
+    for(let card of nikeLaunchList){
+        bot.sendMessage(chatId, "[SNKRS] "+card.name+"\n"+card.link);
     };
     
     // 뉴발란스(성인) 발매정보
@@ -50,7 +47,7 @@ task("test",async (done) => {
         const today = (nowDate.getMonth()+1)+"-"+nowDate.getDate();
         const todayLaunchList = [];
         const cardNodes = document.querySelectorAll("#launchingList li") as NodeListOf<HTMLLIElement>;
-        if(!cardNodes.length) return [];
+        if(!cardNodes.length) return [{name:"오늘 발매없음",link:""}];
         cardNodes.forEach((li) => {
             // props
             const monthEl = li.querySelector(".lMonth") as HTMLSpanElement;
@@ -69,20 +66,17 @@ task("test",async (done) => {
                 });
             }
         });
+        if(!todayLaunchList.length) return [{name:"데이터 미수급 에러",link:""}];
         return todayLaunchList;
     });
 
-    if(nbLaunchList.length){
-        // 오늘발매 있음
-        for(let card of nbLaunchList){
-            bot.sendMessage(chatId, "[NB] "+card.name+"\n"+card.link);
-        };
-    }else{
-        bot.sendMessage(chatId, "[NB] 뉴발란스(성인) 오늘 발매없음");
+    // 텔레그램 발송
+    for(let card of nbLaunchList){
+        bot.sendMessage(chatId, "[NB] "+card.name+"\n"+card.link);
     };
 
-    // await new Promise((resolve)=>setTimeout(resolve,660000)); // 11분 후 닫음 (텔레그램은 연결 후 10분 이내 끊을경우 429에러 발생함 , Error: ETELEGRAM: 429 Too Many Requests: retry after 599)
-    await new Promise((resolve)=>setTimeout(resolve,1000)); // 개발용 1초 있다가 닫음 (그냥 에러 나는걸로..)
+    await new Promise((resolve)=>setTimeout(resolve,660000)); // 11분 후 닫음 (텔레그램은 연결 후 10분 이내 끊을경우 429에러 발생함 , Error: ETELEGRAM: 429 Too Many Requests: retry after 599)
+    // await new Promise((resolve)=>setTimeout(resolve,1000)); // 개발용 1초 있다가 닫음 (그냥 에러 나는걸로..)
     await browser.close();
     // bot.sendMessage(chatId, "닫습니다");
     await bot.close();
